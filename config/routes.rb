143ff3abe23c7ac9c34bc452af84de60a274dd44
Karl Amoguis <ktamoguis@gmail.com>
2018-07-07 07:53:21 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
-  get '/students/new', to: 'students#new', as: 'new_student'
+  resources :students
+  #only: :index
+  #get '/students/:id', to: 'students#show', as: 'student'
+  #get '/students/new', to: 'students#new', as: 'new_student'
+  #post '/students
 end
